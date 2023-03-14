e962adce51f4eb090e0f021db123d42c41ab009e
Anilreddy2009 <116097932+Anilreddy2009@users.noreply.github.com>
2023-03-14 14:12:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require ("express");
 const mongoose=require ("mongoose");
 const router=require("./routers/news");
 const app=express();
-const url="mongodb+srv://Anilreddy_2009:Anilreddys1@cluster0.ili4rd0.mongodb.net/?retryWrites=true&w=majority";
+const url="mongodb+srv://Anilreddy_2009:Anilreddys1@cluster0.mgsx3oj.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
 const con=mongoose.connection
