dd4a20d6d4bc13e8cca7622c4a3b76a835e1a26b
Anilreddy2009 <116097932+Anilreddy2009@users.noreply.github.com>
2023-03-14 15:45:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require ("express");
 const mongoose=require ("mongoose");
-const router=require("./routers/news");
+const News=require('./newsModel');
 const app=express();
 const url="mongodb+srv://Anilreddy_2009:Anilreddys1@cluster0.mgsx3oj.mongodb.net/?retryWrites=true&w=majority";
 
@@ -10,10 +10,34 @@ app.use(express.json())
 app.get("/",(req,res)=>{
     res.send('working');
 })
+
 con.on('open',()=>{
     console.log('connected....')
 })
-app.use("/n/news",router)
+
+app.get("/news",async(req,res)=>{
+    try{
+        const news=await News.find()
+        res.json(news);
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.post("/news",async(req,res)=>{
+    const news=new News({
+        title:req.body.title,
+        description:req.body.description,
+        url:req.body.url,
+        urlToImage:req.body.urlToImage
+    })
+    try{
+        const n1=await news.save();
+        res.json(n1);
+    }catch(err){
+        res.send(err)
+    }
+})
 app.listen(9000,()=>{
     console.log("server started");
 })
